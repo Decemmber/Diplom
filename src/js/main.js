63973a052847bd861eaa4e537dabbbcf3385a421
Decemmber <eu.krimp29@gmail.com>
2019-06-04 19:07:59 +0700

    new rep

---
+++ src/js/main.js
@@ -0,0 +1,70 @@
+/*slider*/
+$(document).ready(function() {
+   $('.multiple-items').slick({
+       lazyLoad: 'ondemand',
+       infinite: true,
+       slidesToShow: 4,
+       slidesToScroll: 2,
+       arrows: false,
+       dots: true,
+       dotsClass: 'dots-style',
+       responsive: [
+           {
+             breakpoint: 1024,
+             settings: {
+               slidesToShow: 3,
+               slidesToScroll: 2,
+             }
+           },
+           {
+             breakpoint: 767,
+             settings: {
+               slidesToShow: 2,
+               slidesToScroll: 2
+             }
+           },
+           {
+             breakpoint: 480,
+             settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1
+             }
+           },
+           {
+               breakpoint: 479,
+               settings: {
+                 slidesToShow: 1,
+                 slidesToScroll: 1
+               }       
+           }]
+     });
+
+/*Scroll button*/
+$(window).scroll(function () {
+    if ($(this).scrollTop() != 0)
+       $('#toTop').fadeIn();
+    else
+       $('#toTop').fadeOut();
+ });
+ $('#toTop').click(function () {
+    $('body,html').animate({
+       scrollTop: 0
+    }, 800);
+ });
+
+ 
+ $('.menu-icon').click(function () {
+  $('nav').slideToggle(500);
+  $('.menu__items').css({
+     'display': 'flex',
+     'flex-direction': 'column'
+  });
+
+  if ($('.menu-icon').html() == '<i class="fas fa-times"></i>') {
+     $(this).html('<i class="fas fa-bars"></i>');
+  } else {
+     $(this).html('<i class="fas fa-bars"></i>');
+  }
+});})
+
+
