3f30be15b8287f7fb5b83fc689632e79437a1fa9
Decemmber <eu.krimp29@gmail.com>
2019-06-13 18:56:24 +0700

    +var li

--- firebase.js
+++ firebase.js
@@ -1,3 +1,7 @@
+var registration_li = document.getElementById('registration_li');
+var signin_li = document.getElementById('signin_li');
+var lk_li = document.getElementById('lk_li');
+
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       console.log('user', user);
